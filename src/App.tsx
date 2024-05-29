ea973a4c9c21de9feacb0b98a26a0ed95316df7e
Tung Dinh <tungxd301@gmail.com>
2024-05-29 15:30:20 -0700

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <HomePage />,
     },
   ]);
 
